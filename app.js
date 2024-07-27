e47b49dc6f5ab13f9cc319e23707d2c514f24396
david <jimeenzpenadavid978@gmail.com>
2024-07-26 23:12:44 -0500

    "Cambiar el texto del botón de copiar para que, al copiar, muestre "Texto copiado" y vuelva al original después de 2 segundos."

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function encrypt() {
                                  .replace(/o/g, "ober")
                                  .replace(/u/g, "ufat");
     document.getElementById('outputText').value = encryptedText;
-    
+    document.getElementById('inputText').value = '';
     // Activar la tarjeta
     activateCard();
 }
@@ -38,7 +38,8 @@ function decrypt() {
     } else {
         alert("No se pudo desencriptar el texto. Asegúrate de que sea un texto encriptado válido.");
     }
-
+    document.getElementById('inputText').value = '';
+    
     // Activar la tarjeta
     activateCard();
 }
@@ -56,11 +57,23 @@ function activateCard() {
 
 function copyToClipboard() {
     var outputText = document.getElementById('outputText');
+    var button = document.querySelector('.button-copy');
+
     outputText.select();
     outputText.setSelectionRange(0, 99999); // Para dispositivos móviles
 
+    // Usa la API del portapapeles para copiar el texto
     navigator.clipboard.writeText(outputText.value)
         .then(() => {
+            // Cambia el texto del botón a "Texto copiado"
+            button.textContent = 'Texto copiado';
+            button.classList.add('copied'); 
+
+            // Restaura el texto del botón después de 2 segundos
+            setTimeout(function() {
+                button.textContent = 'Copiar';
+                button.classList.remove('copied'); 
+            }, 2000);
         })
         .catch(err => {
             console.error("Error al copiar el texto: ", err);
